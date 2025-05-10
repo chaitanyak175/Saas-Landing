0f8f41246c421d8bbdfeabd02b4b382a5943a487
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-05-10 16:36:01 +0530

    made a few more sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Header } from "./sections/Header";
 import { Hero } from "./sections/Hero";
 import { LogoTicker } from "./sections/LogoTicker";
 import { ProductShowcase } from "./sections/ProductShowcase";
+import { Pricing } from "./sections/Pricing";
+import { Testimonials } from "./sections/Testimonials";
+import { CallToAction } from "./sections/CallToAction";
 
 export default function Home() {
     return (
@@ -11,6 +14,9 @@ export default function Home() {
             <Hero />
             <LogoTicker />
             <ProductShowcase />
+            <Pricing />
+            <Testimonials />
+            <CallToAction />
         </React.Fragment>
     );
 }
--- app/sections/ProductShowcase.tsx
+++ app/sections/ProductShowcase.tsx
@@ -1,21 +1,47 @@
 import ProductImage from "@/app/assets/product-image.png";
+import PyramidImage from "@/app/assets/pyramid.png";
+import TubeImage from "@/app/assets/tube.png";
+
 import Image from "next/image";
 
 export const ProductShowcase = () => {
     return (
-        <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+        <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
             <div className="container">
-                <div className="flex justify-center">
-                    <div className="tag">Boost your productivity</div>
+                <div className="section-heading">
+                    <div className="flex justify-center">
+                        <div className="tag">Boost your productivity</div>
+                    </div>
+                    <h2 className="section-title mt-5">
+                        A more effective way to track progress
+                    </h2>
+                    <p className="section-descripton mt-5">
+                        Effortlessly turn your ideas into a fully functional,
+                        responsive Saas website in just minutes with this
+                        template.
+                    </p>
                 </div>
-                <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">
-                    A more effective way to track progress
-                </h2>
-                <p className="text-center">
-                    Effortlessly turn your ideas into a fully functional,
-                    responsive Saas website in just minutes with this template.
-                </p>
-                <Image src={ProductImage} alt="Product Image" />
+                <div className="relative">
+                    <Image
+                        src={ProductImage}
+                        alt="Product Image"
+                        className="mt-10"
+                    />
+                    <Image
+                        src={PyramidImage}
+                        alt="Pyramid Image"
+                        height={262}
+                        width={262}
+                        className="hidden md:block md:absolute -right-36 -top-32"
+                    />
+                    <Image
+                        src={TubeImage}
+                        alt="Tube Image"
+                        height={248}
+                        width={248}
+                        className="hidden md:block md:absolute bottom-24 -left-36"
+                    />
+                </div>
             </div>
         </section>
     );
