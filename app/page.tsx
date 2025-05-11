72646e676bfd24c9f64b4075babe1309adf7e3c4
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-05-11 18:16:57 +0530

    added a fix for the linting issue

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,8 @@ export default function Home() {
             cancelAnimationFrame(animationId);
         };
     }, []);
+
+    if (!mounted) return null;
     return (
         <React.Fragment>
             <Header />
